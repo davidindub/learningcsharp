b29f664777f806dd7a53348892d897e3eb3d153a
David <69420622+davidindub@users.noreply.github.com>
2022-08-11 22:10:07 +0000

    Completed Functions section

--- Start/Functions/Basics/Program.cs
+++ Start/Functions/Basics/Program.cs
@@ -5,12 +5,26 @@
 
 // TODO: Functions have a return type, name, and optional parameters
 
+float MilesToKm(float miles) {
+  float result = miles * 1.6f;
+  return result;
+}
 
 // TODO: A function with no return value has a 'void' type
 
+void PrintWithPrefix(string theStr) {
+  Console.WriteLine($"::> {theStr}");
+}
 
 // TODO: Call first function
 
+Console.WriteLine($"8 Miles is {MilesToKm(8.0f)}km");
+Console.WriteLine($"3 Miles is {MilesToKm(3.0f)}km");
+Console.WriteLine($"10 Miles is {MilesToKm(10.0f)}km");
 
 // TODO: Call second function
+
+PrintWithPrefix("Test String");
+PrintWithPrefix("Another Test String");
+PrintWithPrefix("Finished!!");
 
--- Start/Functions/RefAndOutParams/Program.cs
+++ Start/Functions/RefAndOutParams/Program.cs
@@ -5,13 +5,24 @@ namespace RefAndOutParams {
         // TODO: Ordinary value arguments cannot be modified by the function
         // because they are a copy of the original value
         static void TestFunc1(int arg1) {
+          arg1 += 10;
 
-            Console.WriteLine($"{arg1}");
+          Console.WriteLine($"TestFunc1: {arg1}");
         }
+
+        static void TestFunc2(ref int arg1) {
+          arg1 += 10;
+
+          Console.WriteLine($"TestFunc2: {arg1}");
+        }
+
+      static void PlusTimes(int arg1, int arg2, out int sum, out int product) {
+        sum = arg1 + arg2;
+        product = arg1 * arg2;
+      }
 
         // TODO: Arguments that are passed by reference can be modified
         // by the function and reflected back to the caller
-
 
         // TODO: The "out" keyword means that the parameter returns a value and is not
         // used to supply data to the function
@@ -23,10 +34,19 @@ namespace RefAndOutParams {
 
             // Functions don't normally modify value arguments
             TestFunc1(val1);
-            Console.WriteLine($"{val1}");
+            Console.WriteLine($"after TestFunc1: {val1}");
 
             // TODO: Using the "ref" keyword, arguments can be passed by reference
             // which allows the function to modify them
+
+          TestFunc2(ref val1);
+          Console.WriteLine($"after TestFunc2: {val1}");
+
+          int a, b;
+
+          PlusTimes(val1, val2, out a, out b);
+
+          Console.WriteLine($"{a}, {b}");
 
 
             // TODO: The "out" keyword can be used to indicate that an argument
