5cbcbb31545270364b4063901cac4e581f817db3
David <69420622+davidindub@users.noreply.github.com>
2022-08-11 10:20:54 +0000

    Add example files from LinkedIn Learning course

--- Program.cs
+++ Program.cs
@@ -1,34 +1,78 @@
 using System;
 
-namespace Exceptions
+namespace Operations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 1002;
-            int y = 3;
-            int result;
+            // Declare some strings for the exercises
+            string outstr;
+            string str1 = "The quick brown fox jumps over the lazy dog.";
+            string str2 = "This is a string";
+            string str3 = "THIS is a STRING";
+            string[] strs = {"one", "two", "three", "four"};
 
-            // TODO: try-catch expressions make error checking easier
-          try {
-            if (x>1000) {
-              throw new ArgumentOutOfRange("x", "x has to be 1000 or less");
-            }
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
-          }
-          catch (DivideByZeroException e) {
-            Console.WriteLine("Whoops!");
-            Console.WriteLine(e.Message);
-          }
-          catch (ArgumentOutOfRangeException e) {
-            Console.WriteLine("Sorry 1000 is the limit");
-            Console.WriteLine(e.Message);
-          }
-          finally {
-            Console.WriteLine("This code will always run");
-          }
+            // TODO: Length of a string 
+          Console.WriteLine(str1.Length);
+
+
+            // TODO: Access individual characters
+          Console.WriteLine(str1[14]);
+
+
+            // TODO: iterate over a string like any other sequence of values
+            foreach (Char ch in str1) {
+                Console.Write(ch);
+                if (ch == 'b') {
+                    Console.WriteLine();
+                    break;
+                }
+            } 
+
+
+            // TODO: String Concatenation    
+          outstr = String.Concat(strs);
+          Console.WriteLine(outstr);
+
+
+            // TODO: Joining strings together with Join
+
+          outstr = String.Join(".", strs);
+          Console.WriteLine(outstr);
+
+          outstr = String.Join("---", strs);
+          Console.WriteLine(outstr);
+
+
+            // TODO: String Comparison
+            // Compare will perform an ordinal comparison and return:
+            // < 0 : first string comes before second in sort order
+            // 0 : first and second strings are same position in sort order
+            // > 0 : first string comes after the second in sort order
+          int result = String.Compare(str2, "This is a string");
+          Console.WriteLine(result);
+
+       
+
+
+            // TODO: Equals just returns a regular Boolean
+
+                    bool isEqual = str2.Equals(str3);
+          Console.WriteLine(isEqual); // Returns False
+
+
+            // TODO: String Searching
+
+          Console.WriteLine(str1.IndexOf("e"));
+          Console.WriteLine(str1.IndexOf("fox"));
+
+          Console.WriteLine(str1.LastIndexOf("e"));
+
+          outstr = str1.Replace("fox", "cat");
+          Console.WriteLine(outstr);
+          Console.WriteLine(outstr.IndexOf("fox"));
+
         }
     }
 }
