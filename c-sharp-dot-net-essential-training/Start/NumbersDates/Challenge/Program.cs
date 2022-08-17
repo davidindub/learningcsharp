d220b6c2cbed278ab051fe886b4d006314229534
David Kelly <davidpjckelly@gmail.com>
2022-08-17 17:16:34 +0100

    Fix linked repo

---
+++ c-sharp-dot-net-essential-training/Start/NumbersDates/Challenge/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime today = DateTime.Today;
+string response;
+
+
+do
+{
+    Console.WriteLine($"Which date? (or 'exit') \n");
+    response = Console.ReadLine();
+
+    if (response == "exit")
+    {
+        break;
+    }
+
+    DateTime parsedDate;
+    TimeSpan daysBetween;
+
+    if (DateTime.TryParse(response, out parsedDate))
+    {
+
+        if (parsedDate == today)
+        {
+            Console.WriteLine($"That's today! {parsedDate:d}.");
+
+        }
+        else if (parsedDate < today)
+        {
+            daysBetween = today - parsedDate;
+            Console.WriteLine($"That day went by {daysBetween.Days} ago!");
+        }
+        else
+        {
+            daysBetween = parsedDate - today;
+            Console.WriteLine($"That will be in {daysBetween.Days} days!");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"That doesn't seem to be a valid date!");
+
+
+    }
+} while (response != "exit");
---
+++ c-sharp-dot-net-essential-training/Start/NumbersDates/FormatNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+// Example file for formatting numerical data in .NET
+
+int[] quarters = { 1, 2, 3, 4 };
+int[] sales = { 100000, 150000, 200000, 225000 };
+double[] intlMixPct = { .386, .413, .421, .457 };
+int val1 = 1234;
+decimal val2 = 1234.5678m;
+
+// TODO: Specifying Numerical formatting
+// General format is {index[,alignment]:[format]}
+// Common types are N (Number), G (General), F (Fixed-point), 
+// E (Exponential), D (Decimal), P (Percent), X (Hexadecimal),
+// C (Currency in local format)
+
+// Console.WriteLine($"Decimal: {val1:D}, Number: {val1:N}, Fixed-point: {val1:F}, General: {val1:G}");
+// Console.WriteLine($"Exponential: {val2:E}, Number: {val2:N}, Fixed-point: {val2:F}, General: {val2:G}");
+
+// TODO: Add a number after the format to specify precision
+
+// Console.WriteLine($"Decimal 6 digits: {val1:D6}, Number 2 digits: {val1:N2}, Fixed-point 1 digit: {val1:F1}, General 3 digits: {val1:G3}");
+
+
+
+// TODO: Formatting with alignment and spacing
+Console.WriteLine("Sales by Quarter:");
+Console.WriteLine($"{quarters[0],12} {quarters[1],12} {quarters[2],12} {quarters[3],12}");
+Console.WriteLine($"{sales[0],12:C0} {sales[1],12:C0} {sales[2],12:C0} {sales[3],12:C0}");
+Console.WriteLine();
+Console.WriteLine("International Sales:");
+Console.WriteLine($"{intlMixPct[0],12:P0} {intlMixPct[1],12:P0} {intlMixPct[2],12:P1} {intlMixPct[3],12:P2}");
