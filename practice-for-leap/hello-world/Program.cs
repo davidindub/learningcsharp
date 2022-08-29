91594f4bf7622bb4809c60ce64a4abe5b9fef763
David Kelly <davidpjckelly@gmail.com>
2022-08-29 11:05:03 +0100

    Add files for 'C# Essential Training 1: Types and Control Flow' LinkedIn Course

---
+++ practice-for-leap/hello-world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            bool LearningToday = true;
+            int num1 = 20;
+            char char4 = 'D';
+            string str1 = "Hello!";
+
+            Console.WriteLine($"{LearningToday} {num1} {char4} {str1}");
+
+
+        }
+    }
+}
